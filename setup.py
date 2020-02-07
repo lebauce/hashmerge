98efb2ea38539644a1ee1f40d98190a9fb0a5b64
Sylvain Baubeau <lebauce@gmail.com>
2020-02-07 13:02:58 +0100

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='hashmerge',
-    version='0.1',
+    version='0.2',
     url='https://github.com/lebauce/hashmerge',
     author='Sylvain Baubeau',
     author_email='bob@glumol.com',
